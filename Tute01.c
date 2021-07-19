529e7dc308286fc53785e9ce0ecfd34afb8ebb17
KeshanDevil <87665135+KeshanDevil@users.noreply.github.com>
2021-07-19 23:01:49 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
-  
+      int m1,m2;
+      printf("input mark");
+      scanf("%d",&m1);
+      printf("input mark");
+      scanf("%d",&m2);
+   int avg;
+   int sum;
+   sum=m1+m2;
+   avg=sum/2;
+   
+   printf("%d",avg);
   return 0;
 }
 
