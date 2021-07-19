9b452ffdcae4a6bddeb1bb21f82c74fae808aaff
KeshanDevil <87665135+KeshanDevil@users.noreply.github.com>
2021-07-20 00:28:55 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -4,16 +4,16 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+#include <iostream>
 
 int main() {
    int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+   cout<<"Enter a value for no 1 : ";
+   cin>>no1;
+   cout<<"Enter a value for no 2 : ";
+   cin>>no2;
+   cout<<minimum(no1, no2);
+   cout<<maximum(no1, no2);
+   cout<<multiply(no1, no2);
    return 0;
 }
