e20f0ae672f1d8006d798bc3b761cc03fcbc8436
KeshanDevil <87665135+KeshanDevil@users.noreply.github.com>
2021-07-20 00:22:30 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -2,7 +2,7 @@
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
-#include <stdio.h>
+#include <iostream>
 
 int main() {
       int m1,m2;
